98b76c8c78e57d8808ac6b781219dae14a07ba16
LUCIFER-dev-king <nihaljesiya@gmail.com>
2021-08-08 17:06:17 +0530

    added tailwind css

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import "./App.css";
 
 function App() {
-  return <div className='App'>Video Calling App</div>;
+  return (
+    <div className='bg-black max-w-7-xl mx-auto px-2'>Video Calling App</div>
+  );
 }
 
 export default App;
